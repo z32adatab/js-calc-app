50f56d5b1a59c67dafce7215b1f8395fa107df58
Sukhi Grewal <z32adatab@gmail.com>
2021-07-23 01:01:59 -0700

    added fn to update to Display (output)

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ class Calculator{
     }
 
     clear() {
-        this.currentOperand = currentOperand;
-        this.previousOperand = previousOperand;
+        this.currentOperand = '';
+        this.previousOperand = '';
         this.operation = undefined;
     }
 
@@ -16,7 +16,8 @@ class Calculator{
     }
 
     appendNumber(number){
-
+        //this.currentOperand = number;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
     chooseOperations(operations){
@@ -28,7 +29,7 @@ class Calculator{
     }
 
     updateDisplay(){
-
+        this.currentOperandTextElement.innerText = this.currentOperand
     }
 
 }
@@ -41,3 +42,11 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand')
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
