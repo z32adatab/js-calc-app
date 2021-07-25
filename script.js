0de147e4d43b4712ee33faa4099bde5dfe344497
Sukhi Grewal <z32adatab@gmail.com>
2021-07-24 19:51:04 -0700

    added functionality to chooseOperation fn.

--- script.js
+++ script.js
@@ -16,11 +16,14 @@ class Calculator{
     }
 
     appendNumber(number){
-        //this.currentOperand = number;
+        if(number === '.' && this.currentOperand.includes('.')) return;
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
-    chooseOperations(operations){
+    chooseOperation(operation){
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = ''; 
 
     }
 
@@ -30,6 +33,7 @@ class Calculator{
 
     updateDisplay(){
         this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
     }
 
 }
@@ -47,6 +51,13 @@ const calculator = new Calculator(previousOperandTextElement, currentOperandText
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
     })
 })
