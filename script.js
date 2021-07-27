1aa9a4b5d145d343052c4f6ad0c23b7d4519dd36
SukhiGrewal <sgrewal@yccd.edu>
2021-07-26 20:54:59 -0700

    Added getDisplayNumber helper method

--- script.js
+++ script.js
@@ -57,9 +57,20 @@ class Calculator{
         this.previousOperand = ''
     }
 
+    getDisplayNumber(number) {
+        const floatNumber = parseFloat(number)
+        if(isNaN(floatNumber)) return ''
+        return floatNumber.toLocaleString('en')
+    }
+
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = 
+            this.getDisplayNumber(this.currentOperand)
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText = 
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        }
+        
     }
 
 }
