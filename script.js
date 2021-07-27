70bc0631ca89d7ea6e507486cffdfd7a6438b61e
SukhiGrewal <sgrewal@yccd.edu>
2021-07-26 21:13:56 -0700

    add fn to split input and updated UI

--- script.js
+++ script.js
@@ -58,9 +58,24 @@ class Calculator{
     }
 
     getDisplayNumber(number) {
-        const floatNumber = parseFloat(number)
-        if(isNaN(floatNumber)) return ''
-        return floatNumber.toLocaleString('en')
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay 
+        if (isNaN(integerDigits)){
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('eng', { maximumFractionDigits: 0 })
+
+        }
+
+        if (decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+
+    
     }
 
     updateDisplay(){
@@ -69,6 +84,8 @@ class Calculator{
         if(this.operation != null){
             this.previousOperandTextElement.innerText = 
                 `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
         }
         
     }
